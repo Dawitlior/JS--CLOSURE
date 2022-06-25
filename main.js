e579052cd86cbfd72ebbc3298e4f30914f0e26cd
Dawitlior <dawitlior777@gmail.com>
2022-06-25 16:12:57 +0300

    add

---
+++ main.js
@@ -0,0 +1,40 @@
+//CLOSURE
+//משתנים שהוגדרו בסקופ האב של הפונקציה אם קיים ,יהיו מוכרים בסקופ הבן
+//זו תכונה חשובה שמאפשרת גמישות אבל מסוכנת
+1;
+function myFunction() {
+  console.log(foo);
+}
+let foo = "Hello";
+myFunction();
+
+2;
+
+function myFunc() {
+  const answer = a + b;
+  return answer;
+}
+let a = 5;
+let b = 2;
+let c = myFunc();
+console.log(c);
+
+3;
+if (x == 5) {
+  console.log(five);
+} else {
+  console.log(four);
+}
+let x;
+//-----------------------------------------------------------------
+//במקרה הזה המשתנה פוו הופך לעצמאי למרות שהגדרנו מחוץ לסקופ משתנה עם אותו שם
+
+function someFunction(){
+    let fo = 2;
+    const answer = foo + bar;
+    return answer;
+}
+let fo = 1000;
+let bar = 2;
+let bazi = someFunction();
+console.log(bazi);
